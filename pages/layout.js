d1099422650432e277e2c6ec60d7f1691abf6342
605690919 <229026@whut.edu.cn>
2017-01-07 09:22:58 +0800

    js
    bootstrap3.3.7，headroom，jquery等js文件

---
+++ pages/layout.js
@@ -0,0 +1,11 @@
+﻿var elem = document.querySelector("header");
+var headroom = new Headroom(elem, {
+    "tolerance": 1,
+    "offset": 100,
+    "classes": {
+        "initial": "headroom",
+        "pinned": "headroom--pinned",
+        "unpinned": "headroom--unpinned"
+    }
+});
+headroom.init();
